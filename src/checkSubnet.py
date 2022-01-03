827749ef7d5a8455586f363d7c87e80e45024efb
Tony <32371862+sktonyca@users.noreply.github.com>
2022-01-02 21:02:51 -0500

    Added Additional Exception

--- src/checkSubnet.py
+++ src/checkSubnet.py
@@ -8,6 +8,8 @@ def checkSubnet():
             get_Input = input("Please enter the IPv4 address with its subnet\nExample: 192.168.0.0/28\n")
             myip = ipaddress.IPv4Network(get_Input)
             break
+        except ValueError:
+            print("Input is invalid")
         except ipaddress.AddressValueError:
             print("The ip address is invalid")
         except ipaddress.NetmaskValueError:
