bd5ceb54ef99c2457ea9596c3c27b7178812df87
Tony <32371862+sktonyca@users.noreply.github.com>
2022-02-03 20:00:48 -0500

    Changed Output Format

--- src/checkSubnet.py
+++ src/checkSubnet.py
@@ -8,10 +8,10 @@ def checkSubnet():
             get_Input = input("Please enter the IPv4 address with its subnet\nExample: 192.168.0.0/28\n")
             myip = ipaddress.IPv4Network(get_Input)
             break
-        except ValueError:
-            print("Input is invalid")
-        except ipaddress.AddressValueError:
+        except ipaddress.AddressValueError: # If ip address is invalid
             print("The ip address is invalid")
-        except ipaddress.NetmaskValueError:
+        except ipaddress.NetmaskValueError: # If subnet mask is invalid
             print("The subnet mask is invalid")
+        except ValueError: # If the input is invalid
+            print("Input is invalid")
     return get_Input,myip
--- src/printResult.py
+++ src/printResult.py
@@ -5,6 +5,8 @@ def printResult(ip, thisNetwork):
     print("Information for {0} network".format(ip))
     print("{0}".format("-"*75))
     print("Network Address | Broadcast Address | Total Addresses | Usable Addresses\n{0:<15} | {1:<17} | {2:<15} | {3}\n".format(str(thisNetwork.network_address),str(thisNetwork.broadcast_address), thisNetwork.num_addresses, len(list(thisNetwork.hosts()))))
-    print("Printing usable addresses")
-    for host in list(thisNetwork.hosts()):
-        print(host)
+    if (len(list(thisNetwork.hosts()))>5):
+        print("Printing first 5 usable addresses")
+    for count, host in enumerate(list(thisNetwork.hosts())):
+        if count <5:
+            print(host)
